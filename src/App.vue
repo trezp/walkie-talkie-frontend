3c3807b5af41466050d2fe08883c3e2fa8947f95
Treasure Porth <treasure.rose@gmail.com>
2021-11-20 11:30:58 -0800

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button v-if="status === ready">Press To Talk</button>
+    <div v-else>
+      <p>Enter your name to begin.</p>
+      <form>
+        <input type="text" placeholder="What's your name?">
+        <input type="submit" value="Begin Session">
+      </form>
+    </div>
+    <p>{{ status }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Device } from 'twilio-client';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+      identity: '',
+      status: 'There is no status',
+      ready: false
+    }
+  },
+  methods: {
+    setUp(event){
+      event.preventDefault();
+    }
+  },
+  mounted(){
+    const device = new Device();
+    console.log(device)
+
+    device.on('incoming', connection => {
+      // immediately accepts incoming connection
+      connection.accept();
+      this.status = connection.status();
+    });
+
+    device.on('ready', () => {
+      this.status = "device ready"; 
+      this.ready = true; 
+    });
+
+    device.on('connect', connection => {
+      this.status = connection.status();
+    });
+
+    device.on('disconnect', connection => {
+      this.status = connection.status();
+    });
+  },
+  components: {}
 }
 </script>
 
